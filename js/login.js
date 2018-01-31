b3e413bf0e0fb8327a8f0a267887a7d691c2803b
JianWind <35483711+JianWind@users.noreply.github.com>
2018-01-31 17:25:27 +0800

    添加了登录功能
    未完成状态，登陆失败

--- js/login.js
+++ js/login.js
@@ -9,9 +9,57 @@ summerready = function(){
 	    $(this).toggleClass('eye');  		    
 	})
 	$(".um-btn").click(function(){
-	var username = $(".uname").val();
-	var 
-	
+	var username = $("#uname").val();
+	var password = $("#pword").val();
+	if( username == "" || undefined){
+			summer.toast({
+                 "msg" : "用户名不能为空!" 
+            });
+			return;
+		}
+	if( password == "" || undefined){
+			summer.toast({
+                 "msg" : "密码不能为空!" 
+            });
+			return;
+		}
+		
+		summer.writeConfig({
+            "host" : "10.176.120.161",//MA主机地址 
+            "port" : "8088" //MA主机端口
+        });
+		
+		summer.callService("UMService.login", {
+			"appid" : "SKProject001",
+			"callback" : "loginCallBack()",
+			"error" : "loginErrCallBack()",
+			"actionid" : "loginNC",
+			"isDataCollect" : "true",
+			"method" : "UMService.login",
+			"type" : "nc",
+			"user" : username,
+			"pass" : password,
+			"contextmapping" : "none",
+			"funcode" : "summerDemo"  // 在NC端创建一个功能节点,具体查看官方文档ma登录NC 
+		}, false);
 	
 	})
 }
+
+function loginCallBack(args){
+	alert("登录成功: "+JSON.stringify(args));
+	summer.openWin({
+        "id" : "index",
+        "url" : "html/index.html",
+        "pageParam" : {
+            "count" : 1,
+            "uName": $("#uname").val()
+        }
+    });
+	
+}
+
+function loginErrCallBack(args){
+	alert("登录失败: "+JSON.stringify(args));
+}
+
--- ma/src/com/yonyou/skproject/ExampleController.java
+++ ma/src/com/yonyou/skproject/ExampleController.java
@@ -1,5 +1,24 @@
 package com.yonyou.skproject;
 
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.yonyou.uap.um.context.util.UmContextUtil;
+import com.yonyou.uap.um.gateway.service.GatewayServiceFactory;
+import com.yonyou.uap.um.gateway.service.IGatewayService;
+import com.yonyou.uap.ump.log.UmpLogger;
+
 public class ExampleController {
 
+	public String testHttp(String args) throws Exception{
+		System.err.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+	    JSONObject json = new JSONObject(args);
+	    Map<String, String> map = UmContextUtil.transJsonToMap(json);
+	    String appid = map.get("appid");
+	    IGatewayService service = GatewayServiceFactory.findGatewayService(appid, "ncLoginService", map);
+	    return (String)service.doService();
+	}
+	
+	
 }
